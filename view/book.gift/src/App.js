1203bac4ea9c8b2268d1a4a5015cd1fae32e3dc8
kaustubh <kaustubhpardhi0062gmail.com>
2022-11-20 19:01:27 +0530

    shipped navbar with modification

--- view/book.gift/src/App.js
+++ view/book.gift/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero Section/Hero";
 function App() {
   return (
     <div className="App ">
       <Navbar />
+      <Hero/>
     </div>
   );
 }
