e6a0a28e23f818cc0e3c5cbd3b3cd2500bd5d80c
kaustubh <kaustubhpardhi0062gmail.com>
2022-11-25 16:53:00 +0530

    shipped hero section lessgoooo

--- view/book.gift/src/Components/Hero Section/Hero.js
+++ view/book.gift/src/Components/Hero Section/Hero.js
@@ -5,10 +5,13 @@ function Hero(){
     return(
       <div className = "hero">
         <div className="hero-text-content">
-          <h1 className="text-content">Let your audience fill your bookshelves.</h1>
-          <h3 className="text-content">Book.Gift makes supporting easy and fun.</h3>
-          <h3 className="text-content">In just a couple of taps, your fans can buy you a book and leave a message.</h3>
+          <h1 className="text-content text-content-head">Let your audience fill your bookshelves🎁</h1>
+          <h3 className="text-content text-content-mid">In just a couple of taps, your fans can buy you a book and leave a message.</h3>
+          <h3 className="text-content text-content-low">Book.Gift makes supporting easy and fun.</h3>
+          <button className="start-my-page-button">Start my page 🚀</button>
+
         </div>
+
         <div className="illustration-hero ">
             <img src={hero} className="illustration-hero-image" alt= "illustration_hero"/>
         </div>
